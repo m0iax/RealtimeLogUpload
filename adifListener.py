6810bfde4cbd99d187b31c9d710a395616e4857b
m0iax <50994524+m0iax@users.noreply.github.com>
2020-05-04 13:04:16 +0100

    added ui

---
+++ adifListener.py
@@ -0,0 +1,184 @@
+#! /usr/bin/python3
+
+import threading
+from socket import socket, AF_INET, SOCK_DGRAM
+import requests
+import json
+import time
+import configparser
+import os
+#import configAndSettings
+
+def createConfigFile(configFileName):
+    #cretes the config file if it does not exist
+    if not os.path.isfile(configFileName):
+            
+        config = configparser.ConfigParser()
+        config['NETWORK'] = {'serverip': '127.0.0.1',
+                             'serverport': 2333
+                            }
+        config['QRZ.COM'] = {'apikey': 'F1A5-5FF9-6A7F-56AE'
+                            }  
+        config['EQSL.CC'] = {'username': 'username',
+                             'password': 'password'
+                            }  
+        config['SERVICES'] = {'eqsl': 0,
+                             'qrz': 0
+                            }  
+        
+        with open(configFileName, 'w') as configfile:
+            config.write(configfile)
+            configfile.close()
+    
+
+configfilename="./loguploader.cfg"
+createConfigFile(configfilename)
+
+if os.path.isfile(configfilename):
+    config = configparser.ConfigParser()
+    config.read(configfilename)
+
+    serverip = config.get('NETWORK','serverip')
+    serverport = int(config.get('NETWORK', 'serverport'))
+
+    qrzAPIKey= config.get('QRZ.COM', 'apikey')
+    
+    qrz=int(config.get('SERVICES','qrz'))
+    qrzEnabled=False
+    if qrz==1:
+        qrzEnabled=True
+    
+    eqsl=int(config.get('SERVICES','eqsl'))
+    eqslEnabled=False
+    if eqsl==1:
+        eqslEnabled=True
+    
+listen = (serverip, serverport)
+
+class UploadServer(threading.Thread):
+    messageType='';
+    messageText=''
+    pttCount=0
+    
+    def toggleEQSL(self):
+        #print('QSL ',self.eqslEnabled)
+        self.eqslEnabled = not self.eqslEnabled
+        #print('QSL Now',self.eqslEnabled)
+        return self.eqslEnabled
+    def toggleQRZ(self):
+        self.qrzEnabled = not self.qrzEnabled
+        return self.qrzEnabled
+    def getQRZEnabled(self):
+        return self.qrzEnabled
+    def getEQSLEnabled(self):
+        return self.eqslEnabled
+    
+    def setQRZEnabled(self, enabled):
+        self.qrzEnabled=enabled
+    def setEQSLEnabled(self, enabled):
+        self.eqslEnabled=enabled
+    def setQRZAPIKey(self, apikey):
+        self.qrzAPIKey=apikey
+        
+    def __init__(self):
+        t = threading.Thread.__init__(self)
+        
+        self.listening = True
+       
+        self.qrzEnabled=qrzEnabled
+        self.eqslEnabled=eqslEnabled
+        
+        self.qrzAPIKey = qrzAPIKey
+        self.first = False
+        self.messageText=None
+        self.messageType=None
+        self.pttCount = 0
+        
+        print('listening on', ':'.join(map(str, listen)))
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.bind(listen)
+
+    def sendToQRZ(self, urlString):
+      
+        self._session = requests.Session()
+        self._session.verify = False
+        r = self._session.get(urlString)
+        if r.status_code == 200:
+            print(r)
+            print(r.text)
+            return True
+        
+        print('Response: '+r)
+        raise Exception("Could not send to QRZ")    
+
+    def sendToEQSL(self, urlString):
+          
+        self._session = requests.Session()
+        self._session.verify = False
+        r = self._session.get(urlString)
+        if r.status_code == 200:
+            print(r)
+            print(r.text)
+            return True
+        
+        print('Response: '+r)
+        raise Exception("Could not send to eQSL")    
+        
+    def uploadToEQSL(self, adifEntry):
+        print('Uploading to eQSL.cc')
+        url='https://www.eQSL.cc/qslcard/importADIF.cfm?ADIFData={0}'
+        url=url+'&EQSL_USER={1}&EQSL_PSWD={2}'
+        url=url.format(adifEntry,self.eqslUser,self.eqslPassword)
+        
+        print('eqsl '+url)
+        self.sendToEQSL(url)
+        
+    def uploadToQRZ(self, logEntry):
+        print('Uploading to QRZ.com')
+        url = ' http://logbook.qrz.com/api'
+        url = url+'?KEY={0}&'
+        url = url+'ACTION=INSERT&ADIF={1}'
+        url = url.format(self.qrzAPIKey, logEntry.decode())
+        
+        print(url)
+        self.sendToQRZ(url)
+    def setListen(self, listen):
+        self.listening=listen
+        if self.listening==False:
+            if self.sock!=None:
+                self.sock.close()
+    def run(self):
+        try:
+            try:
+                while self.listening:
+                    if self.sock!=None:
+                        content, addr = self.sock.recvfrom(65500)
+                    if content!=None and addr!=None:
+                        print('Detected ADIF from JS8Call:', ':'.join(map(str, addr)))
+                    
+                        if self.qrzEnabled:
+                            self.uploadToQRZ(content)
+                        if self.eqslEnabled:
+                            self.uploadToEQSL(content)
+                        if not self.qrzEnabled and not self.eqslEnabled:
+                            print ('No ADIF upload enabled. ADIF not uploaded.')
+                
+            finally:
+                self.sock.close()
+                #self.join()
+        except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+            print ("\nKilling Thread...")
+            self.listening = False
+            self.join() # wait for the thread to finish what it's doing
+            print ("Done.\nExiting.")
+            
+    def close(self):
+        self.listening = False
+
+
+if __name__ == "__main__":
+    
+    server = UploadServer()
+    server.start()
+    
+
---
+++ adifUploader.py
@@ -0,0 +1,120 @@
+#! /usr/bin/python3
+
+from tkinter import *
+import adifListener
+import settings
+from tkinter import Frame
+
+WIDTH=270
+HEIGHT=100
+appName="ADIF Uploader for JS8Call"
+
+class UI(Tk):
+    def loadSettings(self, settingValues):
+        print('Loading settings')
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+            print("Main Window is closing, call any function you'd like here!")
+
+    def __enter__(self):
+            # make a database connection and return it
+        print('Starting')
+    def ask_quit(self):
+        if self.upLoader!=None:
+            print('Shutting down ADIF Listener')
+            self.upLoader.setListen(False)
+            self.upLoader.close()
+            self.upLoader.join()
+            
+        print('Exiting. Thanks for using '+appName+' By M0IAX')
+        
+        self.destroy() 
+    def updateeQSL(self):
+        if self.upLoader!=None:
+            enabled=self.upLoader.toggleEQSL()
+            self.configureEQSLButton(enabled)
+    def updateQRZ(self):
+        if self.upLoader!=None:
+            enabled=self.upLoader.toggleQRZ()
+            self.configureQRZButton(enabled)
+    def configureQRZButton(self, enable):
+        if enable:
+            self.enableQRZButton.configure(bg="green")
+            self.enableQRZButton.configure(text="QRZ Upload Enabled")
+            
+        else:
+            self.enableQRZButton.configure(bg="red")
+            self.enableQRZButton.configure(text="QRZ Upload Disabled")
+            
+
+    def configureEQSLButton(self, enable):
+        if enable:
+            self.enableQSLButton.configure(bg="green")
+            self.enableQSLButton.configure(text="eQSL Upload Enabled")
+            
+        else:
+            self.enableQSLButton.configure(bg="red")
+            self.enableQSLButton.configure(text="eQSL Upload Disabled")
+                
+    def show_buttons(self, frame, controller):
+
+
+        self.enableQRZButton=Button(frame,text='QRZ Upload Disabled', bg="red", command=self.updateQRZ)
+        self.enableQRZButton.grid(row=0, column=0, padx=5, pady=5)
+        
+        
+        self.enableQSLButton=Button(frame,text='Enable eQSL Upload', bg="red", command=self.updateeQSL)
+        self.enableQSLButton.grid(row=0, column=1, padx=5, pady=5)
+        
+        
+  #      gps_page=Button(frame, text="GPS", command=lambda:controller.show_frame(GPSPage), bg="white")
+  #      gps_page.grid(row=2, column=0, sticky=W+E+N+S, padx=5, pady=5)
+
+ #       aprs_page=Button(frame, text="APRS Message", command=lambda:controller.show_frame(MessagePage), bg="white")
+ #       aprs_page.grid(row=2, column=1, sticky=W+E+N+S, padx=5, pady=5)
+ #       settings_page=Button(frame, text="Settings", command=lambda:controller.show_frame(SettingsPage), bg="white")
+ #       settings_page.grid(row=2, column=2, sticky=W+E+N+S, padx=5, pady=5)
+    
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+    
+        self.settingValues = settings.Settings()
+        self.loadSettings(self.settingValues)
+        
+        self.upLoader = adifListener.UploadServer()
+        self.upLoader.daemon = True
+        self.upLoader.start()
+        
+        self.geometry(str(WIDTH)+"x"+str(HEIGHT))
+        self.title(appName+" by M0IAX")
+        
+        #container = Frame(self)
+        #container.pack(side="top", fill="both", expand=True)
+        #container.grid_rowconfigure(0, weight=1)
+        #container.grid_columnconfigure(0, weight=1)
+        
+        buttonFrame=Frame(self)
+        buttonFrame.pack()
+        
+        self.show_buttons(buttonFrame, self)
+        
+        self.configureEQSLButton(self.upLoader.getEQSLEnabled())
+        self.configureQRZButton(self.upLoader.getQRZEnabled())
+        
+
+if __name__=="__main__":
+    
+    try:
+
+        app = UI()
+        app.protocol("WM_DELETE_WINDOW", app.ask_quit)
+        app.mainloop()
+        
+    finally:
+        print('Finally Quit')
+    # if app.gpsl!=None:
+    #     app.gpsl.setReadGPS(False)
+    #     app.gpsl.join()
+    
+    
+    
